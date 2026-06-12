96bad56e2ce7b731212f6bd7e808f29fb1d2616f
MoJianZhe <861785836@qq.com>
2026-06-12 15:21:00 +0800

    链表实现栈

---
+++ link/link_stack.py
@@ -0,0 +1,42 @@
+from dataclasses import asdict, dataclass
+import json
+
+@dataclass
+class LinkNode():
+    pre:'LinkNode'
+    val:None
+    def __init__(self,val):
+        self.pre = None
+        self.val = val
+        
+
+
+@dataclass
+class LinkStack():
+    tail:LinkNode
+    def __init__(self):
+        self.tail=None
+        
+    def push(self,x):
+        a =LinkNode(x)
+        a.pre = self.tail
+        self.tail = a
+
+    def pop(self):
+        val = self.tail.val
+        if(self.tail.pre is not None):
+            self.tail = self.tail.pre
+        return val
+    
+    def peek(self):
+        return self.tail.val
+    
+if __name__=='__main__':
+    s = LinkStack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print(s.pop())
+    print(s.peek())
+    print(s)
+    print(json.dumps(asdict(s)))
