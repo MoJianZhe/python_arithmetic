1eaf0cba21b3d05d830a19469200a235fa144c16
zhishang_fei <zhishang_fei@outlook.com>
2026-06-12 21:03:26 +0800

    环形测试

--- array/array.py
+++ array/array.py
@@ -1,4 +1,16 @@
+def circle():
+    arr = [1,2,3,4,5]
+    i = 0
+    j = 0
+    while i <len(arr):
+        i = (i+1)%len(arr)
+        j = j+1
+        print(j)
+        if(j==1000):
+            break
+
 if __name__ == "__main__":
     print("hello world")
+    circle()
 
 
