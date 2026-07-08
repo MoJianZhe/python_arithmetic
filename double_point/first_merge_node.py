174418ea91f06c4c7fba98c1dba4bf13f5583d5c
MoJianZhe <861785836@qq.com>
2026-07-08 11:21:42 +0800

    成环第一个相交的节点

---
+++ double_point/first_merge_node.py
@@ -0,0 +1,49 @@
+# 142.单链表，环的第一个交点
+# 1、可以保存每个节点的前一个node, 如果前一个有不同的，就是第一个交的
+from link.list_node import ListNode
+
+# 解法二，快，慢指针相交之后，将low指向头节点，让它们继续走(按相同速度），再次相遇的点，就是第一个交点
+def detectCycle(self, head: ListNode):
+    fast = head
+    low = head 
+    while fast is not None and fast.next is not None:
+        low = low.next
+        fast = fast.next.next
+        if low == fast :
+            break
+    if(fast is None or fast.next is None):
+        return None
+    
+    low = head
+    while low is not None:
+        if low == fast :
+            break
+        low = low.next 
+        fast = fast.next
+        # 应该先判断是否相等，再走。可能就是再原点相交。
+        # if low == fast : 
+        #     break
+    return low 
+        
+
+# solution by labula 
+def detectCycle(self, head: ListNode):
+    fast, slow = head, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            break
+    
+    # 上面的代码类似 hasCycle 函数
+    if not fast or not fast.next:
+        # fast 遇到空指针说明没有环
+        return None
+    
+    # 重新指向头结点
+    slow = head 
+    # 快慢指针同步前进，相交点就是环起点
+    while slow != fast:
+        fast = fast.next
+        slow = slow.next
+    return slow
